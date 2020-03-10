4d2ebd416f52dc3d678f9e62d8abb579d376aedf
Jens-Fabian Goetzmann <mail@jefago.com>
2020-03-10 11:31:11 +0100

    Improve testing and HTML demo signup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp');
-// const Rollup = require('rollup');
 const rollup = require('gulp-rollup');
 const size = require('gulp-size');
 const babel = require('rollup-plugin-babel');
@@ -9,9 +8,7 @@ const terser = require('gulp-terser');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 const jestCLI = require('jest-cli');
-// import * as jest from require('gulp-jest');
-
-
+const del = require('del');
 
 const rollupConfig = (production) => { return {
   input: './src/TinyMDE.js',
@@ -23,39 +20,32 @@ const rollupConfig = (production) => { return {
   plugins: [babel()]
 }};
 
-const test = () => jestCLI.run(); /*{
-  // gulp.src('./src/__tests__/*.js')
-  // .pipe(jest());
-  return jestCLI.run();
-}*/
+const clean = () => del(['./dist']);
 
-const jsMax = () => gulp.src('./src/*.js')
-  .pipe(sourcemaps.init())
-  .pipe(rollup(rollupConfig(false)))
-  .pipe(sourcemaps.write())
-  .pipe(rename('tiny-mde.js'))
-  .pipe(size({ showFiles: true }))
-  .pipe(gulp.dest('./dist'));
+const test = () => jestCLI.run([]); 
 
+const jsMax = () => 
+  gulp.src('./src/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(rollup(rollupConfig(false)))
+    .pipe(sourcemaps.write())
+    .pipe(rename('tiny-mde.js'))
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest('./dist'));
 
-const jsMin = () => gulp.src('./src/*.js')
-  .pipe(rollup(rollupConfig(true)))
-  .pipe(terser())
-  .pipe(rename('tiny-mde.min.js'))
-  .pipe(size({ showFiles: true }))
-  .pipe(gulp.dest('./dist'));
+const jsMin = () => 
+  gulp.src('./src/*.js')
+    .pipe(rollup(rollupConfig(true)))
+    .pipe(terser())
+    .pipe(rename('tiny-mde.min.js'))
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest('./dist'));
 
 const js = gulp.series(jsMax, jsMin);
-/*
-  gulp.series(
-    () => 
-    () => gulp.src('./src/tiny-mde.js')
-      .pipe(rollup(rollupConfig(true)))
-      .pipe(rename('pell.min.js'))
-      .pipe(size({ showFiles: true }))
-      .pipe(size({ gzip: true, showFiles: true }))
-      .pipe(gulp.dest('./dist'))
-  );*/
+
+const html = () => 
+  gulp.src('./src/demo.html')
+    .pipe(gulp.dest('./dist'));
 
 const css = () =>
   gulp.src('./src/tiny-mde.css')
@@ -63,18 +53,15 @@ const css = () =>
     .pipe(rename('tiny-mde.min.css'))
     .pipe(gulp.dest('./dist'));
 
-const build = gulp.series(js, css);
-
-const watch = () =>{
-  gulp.watch('./src/*.js', gulp.series(test, js));
+const watch = () => {
+  gulp.watch('./src/*.js', jsMax);
   gulp.watch('./src/*.css', css);
+  gulp.watch('./src/*.html', html);
 }
 
-const dev = gulp.series(
-  test, js, css, watch
-);
-  
-  
+const build = gulp.series(clean, test, js, css, html);
+
+const dev = gulp.series(clean, jsMax, css, html, watch);
 
 exports.default = build;
 exports.dev = dev;
