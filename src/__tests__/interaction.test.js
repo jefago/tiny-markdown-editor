ae976f995a0bd006e19dfbc2cc8774529c0ad0ad
Jens-Fabian Goetzmann <mail@jefago.com>
2020-03-15 17:47:53 +0100

    Test for setting content

--- src/__tests__/interaction.test.js
+++ src/__tests__/interaction.test.js
@@ -1,15 +1,28 @@
 import TinyMDE from '../TinyMDE';
 
-test('sets up correctly when passed an ID', () => {
+beforeEach(() => {
   document.body.innerHTML = '<div id="container"></div>';
+});
+
+test('sets up correctly when passed an ID', () => {
   let tinyMDE = new TinyMDE({element: 'container'});
   expect(document.getElementById('container').firstChild.className).toBe('TinyMDE');
 });
 
 test('sets up correctly when passed an element', () => {
-  document.body.innerHTML = '<div id="container"></div>';
   let tinyMDE = new TinyMDE({element: document.getElementById('container')});
   expect(document.getElementById('container').firstChild.className).toBe('TinyMDE');
 });
 
-// TODO: Tests for passing content, setContent()
+test('Content can be passed to constructor', () => {
+  const content = '# XXXA\nXXXB *XXXC*';
+  const tinyMDE = new TinyMDE({element: 'container', content: content});
+  expect(tinyMDE.getContent()).toEqual(content);
+});
+
+test('Content can be set using setContent()', () => {
+  const content = '# XXXA\nXXXB *XXXC*';
+  const tinyMDE = new TinyMDE({element: 'container'});
+  tinyMDE.setContent(content);
+  expect(tinyMDE.getContent()).toEqual(content);
+});
