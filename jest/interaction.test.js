fa04019649375004426b03208769034a227e1e70
Jens-Fabian Goetzmann <mail@jefago.com>
2021-02-16 12:48:41 +0100

    Add test for paste without focus

--- jest/interaction.test.js
+++ jest/interaction.test.js
@@ -21,3 +21,11 @@ test('Numbered list is continued when pressing enter', async () => {
   await page.keyboard.press('Enter');
   expect(await page.evaluate(() => document.tinyMDE.getContent())).toEqual('1) Line 1\n2) Line 2\n3) ');
 });
+
+test('Pasting works without a focus', async () => {
+  await page.evaluate(() => {
+    document.tinyMDE = new TinyMDE.Editor({element: 'tinymde', content: 'This is\na test'});
+    document.tinyMDE.paste(' for\npasting');
+  });
+  expect(await page.evaluate(() => document.tinyMDE.getContent())).toEqual('This is\na test for\npasting');
+})
