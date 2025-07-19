af5277359bd22b97c1f8f0c5336f6b4354de73cd
Jens-Fabian Goetzmann <mail@jefago.com>
2025-07-19 19:51:20 +0200

    Simplify multi project setup

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,52 @@
 module.exports = {
-  testEnvironment: 'node',
-  globalSetup: './jest/util/jest-global-setup.js',
-  globalTeardown: './jest/util/jest-global-teardown.js',
-  globals: {
-  },
-  setupFiles: [
-    "./jest/util/test-helpers.js"
-  ],
-  setupFilesAfterEnv: [
-    "./jest/util//setup.js"
+  projects: [
+    {
+      displayName: 'chromium',
+      testEnvironment: 'node',
+      globalSetup: './jest/util/jest-global-setup.js',
+      globalTeardown: './jest/util/jest-global-teardown.js',
+      setupFiles: [
+        "./jest/util/test-helpers.js"
+      ],
+      setupFilesAfterEnv: [
+        "./jest/util/multi-browser-setup.js"
+      ],
+      testMatch: ['<rootDir>/jest/**/*.test.js'],
+      globals: {
+        BROWSER_TYPE: 'chromium'
+      }
+    },
+    {
+      displayName: 'firefox',
+      testEnvironment: 'node',
+      globalSetup: './jest/util/jest-global-setup.js',
+      globalTeardown: './jest/util/jest-global-teardown.js',
+      setupFiles: [
+        "./jest/util/test-helpers.js"
+      ],
+      setupFilesAfterEnv: [
+        "./jest/util/multi-browser-setup.js"
+      ],
+      testMatch: ['<rootDir>/jest/**/*.test.js'],
+      globals: {
+        BROWSER_TYPE: 'firefox'
+      }
+    },
+    {
+      displayName: 'webkit',
+      testEnvironment: 'node',
+      globalSetup: './jest/util/jest-global-setup.js',
+      globalTeardown: './jest/util/jest-global-teardown.js',
+      setupFiles: [
+        "./jest/util/test-helpers.js"
+      ],
+      setupFilesAfterEnv: [
+        "./jest/util/multi-browser-setup.js"
+      ],
+      testMatch: ['<rootDir>/jest/**/*.test.js'],
+      globals: {
+        BROWSER_TYPE: 'webkit'
+      }
+    }
   ]
-}
+};
